35ecf7c80c92a57ccdb91cc510b9ac186e73c8a9
Bhavani-R3 <bhavanirameshr3@gmail.com>
2024-01-16 14:07:01 +0530

    chat window content is added

--- src/component/chat/ChatWindow.jsx
+++ src/component/chat/ChatWindow.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import "./css/chatwindow.scss"
+import ChatHeader from './ChatHeader'
+import ChatMessage from './ChatMessage'
+import ChatInput from './ChatInput'
 
 function ChatWindow() {
   return (
     <div className='right-side'>
-      ChatWindow
+      <ChatHeader/>
+      <ChatMessage/>
+      <ChatInput/>
     </div>
   )
 }
--- src/component/chat/SearchUser.jsx
+++ src/component/chat/SearchUser.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import "./css/"
+import "./css/searchUser.scss"
 
 function SearchUser() {
   return (
-    <div>SearchUser</div>
+    <div className='search_chat'>
+      <div>
+        <input type="search" name='user' id='user' placeholder='Search or start new chat' />
+        <i className='bi bi-search'></i>
+      </div>
+    </div>
   )
 }
 
 export default SearchUser
-
